4fdb6d6472c3ebc1c2dd691ff07de59b838fb618
choryang <okee0815@gmail.com>
2020-04-26 05:08:13 +0900

    gif splash screen 구현

--- src/screens/InfoScreen.js
+++ src/screens/InfoScreen.js
@@ -6,7 +6,7 @@ function InfoScreen() {
 
     return (
         <View style={{flex: 1}}>
-            <Image style={{width: '100%', height: '100%', resizeMode: 'cover'}} source={require('../images/splash.gif')} />
+            <Text>부가기능</Text>
         </View>
 
 
